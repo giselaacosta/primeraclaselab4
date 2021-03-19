6672f253077f98e440efc766eb4a5a0d86a90647
gisela <gi_selita2007@hotmail.com>
2021-03-18 22:11:16 -0300

    rr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'login';
+
+  title = 'primerclaseutn';
+  public number1 : number;
+  public number2 : number;
+  public result1 : number;
+  public result2 : number;
+
+  constructor(private location: Location) {}
+  public calc(){
+    this.result1 = (this.number1 + this.number2)/2
+    this.result2 = this.number1 + this.number2
+
+  }
+
+  public limpiar(){
+    location.reload();
+
+
+  }
+
 }
